22f67d5dc130a987316852d7a800118dde3038c5
Truman2557 <144353279+Truman2557@users.noreply.github.com>
2023-11-14 15:14:45 +0000

    still doesnt work

--- script.js
+++ script.js
@@ -1,15 +1,24 @@
-document.getElementById('askButton').addEventListener("click", function(){
+//Create event Listener
+document.getElementById('askButton').addEventListener('click', function () {
     let answer = '';
     let randomIndex = Math.floor(Math.random() * 5);
-    if(randomIndex === 0){
-        let answer = "Yes";
-    } else if(randomIndex === 1){
-        let answer = "No";
-    } else if(randomIndex === 2){
-        let answer = "Ask Again Later";
-    } else if(randomIndex === 3){
-        let answer = "Definately";
-    } else if(randomIndex === 4){
-        let answer = "figure out code later"
+    if (randomIndex === 0) {
+        answer = "It is certain";
+    } else if (randomIndex === 1) {
+        answer = "Ask again later";
+    } else if (randomIndex === 2) {
+        answer = "Ask again later";
+    } else if (randomIndex === 3) {
+        answer = "Ask again later";
+    } else if (randomIndex === 4) {
+        answer = "Ask again later";
+    } else {
+        answer = "Ask again later";
+    }
+    let question = document.getElementById('question').value;
+    if (question.trim() === '') {
+        alert("Please enter a question.");
+    } else {
+        document.getElementById('answer').textContent = answer;
     }
 });
