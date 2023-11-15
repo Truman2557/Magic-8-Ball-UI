9137c570474083b647ce1dd74cb324003728be94
Truman2557 <144353279+Truman2557@users.noreply.github.com>
2023-11-15 14:46:03 +0000

    8 ball now works

--- script.js
+++ script.js
@@ -7,16 +7,17 @@ document.getElementById('askButton').addEventListener('click', function () {
     } else if (randomIndex === 1) {
         answer = "Ask again later";
     } else if (randomIndex === 2) {
-        answer = "Ask again later";
+        answer = "Yes";
     } else if (randomIndex === 3) {
-        answer = "Ask again later";
+        answer = "No";
     } else if (randomIndex === 4) {
-        answer = "Ask again later";
+        answer = "It is uncertain";
     } else {
-        answer = "Ask again later";
+        answer = "Certainly Not";
     }
+
     let question = document.getElementById('question').value;
-    if (question.trim() === '') {
+    if (question.trim() === ''){
         alert("Please enter a question.");
     } else {
         document.getElementById('answer').textContent = answer;
